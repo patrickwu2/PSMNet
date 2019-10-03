0e69ef9373e257732563cebdfc8682f92a531a10
Tsunghan <patrickwu2@gmail.com>
2019-10-03 21:58:24 +0800

    Finish first version inference code

--- inference.py
+++ inference.py
@@ -1,43 +1,34 @@
+from PIL import Image
 import argparse
 import cv2
-
+import os, sys
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
 from models.PSMnet import PSMNet
-from dataloader.KITTI2015_loader import ToTensor, Normalize
+from torch.utils.data import DataLoader
 import torch.nn.functional as F
-
+from dataloader.Scannetv2_loader import Scannetv2
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='PSMNet inference')
 parser.add_argument('--maxdisp', type=int, default=192, help='max diparity')
-parser.add_argument('--left', default=None, help='path to the left image')
-parser.add_argument('--right', default=None, help='path to the right image')
 parser.add_argument('--model-path', default=None, help='path to the model')
-parser.add_argument('--save-path', default=None, help='path to save the disp image')
+parser.add_argument('--datadir', default="/work/kaikai4n/resized_stereo/"
+                    , help='data directory')
 args = parser.parse_args()
 
 
-mean = [0.406, 0.456, 0.485]
-std = [0.225, 0.224, 0.229]
-device_ids = [0, 1, 2, 3]
+#mean = [0.406, 0.456, 0.485]
+#std = [0.225, 0.224, 0.229]
+device_ids = [0]
 device = torch.device('cuda:{}'.format(device_ids[0]))
 
 
 def main():
-    left = cv2.imread(args.left)
-    right = cv2.imread(args.right)
-
-    pairs = {'left': left, 'right': right}
-
-    transform = T.Compose([Normalize(mean, std), ToTensor(), Pad(384, 1248)])
-    pairs = transform(pairs)
-    left = pairs['left'].to(device).unsqueeze(0)
-    right = pairs['right'].to(device).unsqueeze(0)
 
     model = PSMNet(args.maxdisp).to(device)
     if len(device_ids) > 1:
@@ -58,42 +49,35 @@ def main():
     print('3px-error: {}%'.format(state['error']))
 
     model.eval()
+ 
+    val_dataset =  Scannetv2(args.datadir, "dataloader/data_list/scannetv2", 'val')
+    validate_loader = DataLoader(val_dataset, batch_size=1, num_workers=8, collate_fn=val_dataset.customed_collate_fn)
 
-    with torch.no_grad():
-        _, _, disp = model(left, right)
+    N = 0
+    for batch in tqdm(validate_loader):
+        
+        left_img = batch['left'].to(device)
+        right_img = batch['right'].to(device)
+        target_disp = batch['disp'].to(device)
 
-    disp = disp.squeeze(0).detach().cpu().numpy()
-    plt.figure(figsize=(12.84, 3.84))
-    plt.axis('off')
-    plt.imshow(disp)
-    plt.colorbar()
-    plt.savefig(args.save_path, dpi=100)
+        with torch.no_grad():
+            _, _, disp = model(left_img, right_img)
 
-    print('save diparity map in {}'.format(args.save_path))
-
-
-class Pad():
-    def __init__(self, H, W):
-        self.w = W
-        self.h = H
-
-    def __call__(self, sample):
-        pad_h = self.h - sample['left'].size(1)
-        pad_w = self.w - sample['left'].size(2)
-
-        left = sample['left'].unsqueeze(0)  # [1, 3, H, W]
-        left = F.pad(left, pad=(0, pad_w, 0, pad_h))
-        right = sample['right'].unsqueeze(0)  # [1, 3, H, W]
-        right = F.pad(right, pad=(0, pad_w, 0, pad_h))
-        # disp = sample['disp'].unsqueeze(0).unsqueeze(1)  # [1, 1, H, W]
-        # disp = F.pad(disp, pad=(0, pad_w, 0, pad_h))
-
-        sample['left'] = left.squeeze()
-        sample['right'] = right.squeeze()
-        # sample['disp'] = disp.squeeze()
-
-        return sample
-
+        disp = disp.squeeze(0).detach().cpu().numpy()
+        disp_img = Image.fromarray(disp).convert('F')
+        save_path = f'disparity_{N}.tiff'
+        disp_img.save(os.path.join("raw_vis", save_path))
+        #plt.figure(figsize=(8.84, 3.84))
+        #plt.axis('off')
+        #plt.imshow(disp)
+        #plt.colorbar()
+        #save_path = f'disparity_{N}.png'
+        #plt.savefig(os.path.join("validation", save_path), dpi=100)
+        #plt.close()
+        N += 1
+        if N > 10:
+            exit()
+        # print('save diparity map in {}'.format(args.save_path))
 
 if __name__ == '__main__':
     main()
