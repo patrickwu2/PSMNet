560c848449092fd3d8a08e9661afc7e9ae3f0a5d
Tsunghan <patrickwu2@gmail.com>
2019-10-04 12:16:02 +0800

    Finish saving inference file codes.

--- inference.py
+++ inference.py
@@ -1,10 +1,14 @@
 from PIL import Image
 import argparse
 import cv2
-import os, sys
+import os
+from pathlib import Path
+import sys
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
+import numpy as np
+
 from models.PSMnet import PSMNet
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
@@ -19,6 +23,8 @@ parser.add_argument('--maxdisp', type=int, default=192, help='max diparity')
 parser.add_argument('--model-path', default=None, help='path to the model')
 parser.add_argument('--datadir', default="/work/kaikai4n/resized_stereo/"
                     , help='data directory')
+parser.add_argument('--output-dir', required=True)
+parser.add_argument('--batch-size', type=int, default=8)
 args = parser.parse_args()
 
 
@@ -51,11 +57,10 @@ def main():
     model.eval()
  
     val_dataset =  Scannetv2(args.datadir, "dataloader/data_list/scannetv2", 'val')
-    validate_loader = DataLoader(val_dataset, batch_size=1, num_workers=8, collate_fn=val_dataset.customed_collate_fn)
+    validate_loader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=8, collate_fn=val_dataset.customed_collate_fn)
 
     N = 0
     for batch in tqdm(validate_loader):
-        
         left_img = batch['left'].to(device)
         right_img = batch['right'].to(device)
         target_disp = batch['disp'].to(device)
@@ -63,21 +68,15 @@ def main():
         with torch.no_grad():
             _, _, disp = model(left_img, right_img)
 
-        disp = disp.squeeze(0).detach().cpu().numpy()
-        disp_img = Image.fromarray(disp).convert('F')
-        save_path = f'disparity_{N}.tiff'
-        disp_img.save(os.path.join("raw_vis", save_path))
-        #plt.figure(figsize=(8.84, 3.84))
-        #plt.axis('off')
-        #plt.imshow(disp)
-        #plt.colorbar()
-        #save_path = f'disparity_{N}.png'
-        #plt.savefig(os.path.join("validation", save_path), dpi=100)
-        #plt.close()
-        N += 1
-        if N > 10:
-            exit()
-        # print('save diparity map in {}'.format(args.save_path))
+        disp_img = disp.data.cpu().numpy()
+        for img, (scene_name, img_num) in zip(disp_img, batch['name']):
+            save_path = os.path.join(
+                args.output_dir, 
+                scene_name,
+                f'disparity_{img_num}.npy'
+            )
+            path = Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+            np.save(save_path, disp_img)
 
 if __name__ == '__main__':
     main()
